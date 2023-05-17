527bcfbdeaa6643c013c1599561f247f4d2ee493
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-05-17 21:31:09 +0600

    added navbar and footer

---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+// import React from 'react';
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+    const user = 'alu'
+    // const {user, logOut} = useContext(AuthContext)
+
+//   const handleLogout = ()=>{
+//     logOut()
+//     .then(()=>{
+//     })
+//     .catch(err => {
+//       console.log(err) 
+//     })
+//   }
+
+
+    const navItems = <>
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/about'>About</Link></li>
+    {
+      user?.email ? <>
+      <li><Link to='/bookings'>My Bookings</Link></li>
+      <li><button >LogOut</button></li>
+      </> :<li><Link to='/login'>Login</Link></li>  
+      
+    }
+  </>
+    return (
+        <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        {navItems}
+      </ul>
+    </div>
+    <Link to='/' className="btn btn-ghost normal-case text-xl">
+        <img className="h-full w-16 rounded" src='https://images-workbench.99static.com/l20BQsocuxaOUoYnFlWb-1XSOvY=/99designs-contests-attachments/80/80402/attachment_80402256' alt="" />
+    </Link>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {navItems}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn btn-outline btn-warning">APPOINTMENT</a>
+  </div>
+</div> 
+    );
+};
+
+export default Navbar;
