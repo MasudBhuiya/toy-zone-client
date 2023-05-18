0d1d910d10de7c3c2a1f4ba263cb2dab11beb72f
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-05-18 22:33:19 +0600

    added all toys page and get all data from server side

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,9 +1,23 @@
 // import React from 'react';
 
+import { useEffect, useState } from "react";
+import ToysCard from "./ToysCard";
+
 const AllToys = () => {
+    const [items, setItems] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/toys')
+        .then(res => res.json())
+        .then(data => {
+            setItems(data)
+            console.log(data)
+        })
+    },[])
     return (
-        <div>
-            
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 space-y-6 mt-20 mb-20">
+            {
+                items.slice(0, 20).map(item => <ToysCard key={item._id} item={item}></ToysCard>)
+            }
         </div>
     );
 };
